dded902ae1f989ac09894f4fe87aa35f810f2d04
Dmitrii <73357024+cdelicia@users.noreply.github.com>
2023-07-26 14:57:28 -0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -11,7 +11,7 @@ eval $(minikube docker-env)
 
 kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.3/manifests/namespace.yaml
 kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.3/manifests/metallb.yaml
-#kubectl create secret generic -n metallb-system memberlist --from-literal=secretkey="$(openssl rand -base64 128)"
+kubectl create secret generic -n metallb-system memberlist --from-literal=secretkey="$(openssl rand -base64 128)"
 
 docker build -t ftpsdock ./srcs/ftps
 docker build -t mysqldock ./srcs/mysql
